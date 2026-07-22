39eaad821e186386d1cf89f561d36de64b61b85a
af27gx-12 <ampubgmobile@gmail.com>
2026-07-22 13:02:11 +0300

    Use HTTPS URLs

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 app.post("/upload", upload.single("image"), (req, res) => {
-    const url = req.protocol + "://" + req.get("host") + "/uploads/" + req.file.filename;
+    const url = "https://" + req.get("host") + "/uploads/" + req.file.filename;
 
     res.json({
         success: true,
